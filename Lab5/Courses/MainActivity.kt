7e6a368f0f90deb100f27d4aaeb817f40167db24
Arif Ajmal <129920061+arifajmal@users.noreply.github.com>
2024-01-25 13:34:26 +0800

    Add files via upload

---
+++ Lab5/Courses/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.courses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.courses.ui.theme.CoursesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CoursesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+        Topic(R.string.business, 78, R.drawable.business),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.design),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 164, R.drawable.gaming),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Topic(R.string.music, 212, R.drawable.music),
+        Topic(R.string.painting, 172, R.drawable.painting),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.tech, 118, R.drawable.tech)
+    )
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CoursesTheme {
+        Greeting("Android")
+    }
+}
